68bfa2c0c8fa1065f89834fad3f6d65db1b2b7e8
Nodirbek Xushbaxtov <nodirbek.devoloper@gmail.com>
2025-01-14 18:48:59 +0500

    fix get all

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 require('dotenv').config();
+const cors = require('cors');
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors());
 
 // Import routes
 const apiRoutes = require('./routes/api');
