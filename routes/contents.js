38c5430287d88578798ec11886571f6e68b7e7f5
Nodirbek Xushbaxtov <nodirbek.devoloper@gmail.com>
2025-01-14 16:53:32 +0500

    contents added

---
+++ routes/contents.js
@@ -0,0 +1,9 @@
+const authenticateToken = require('../middleware/authenticateToken');
+const router = express.Router();
+const pool = require('../db');
+const express = require("express");
+
+router.get('/getAll', authenticateToken, async (req, res) => {
+    const result = await pool.query('SELECT * FROM contents');
+    res.status(200).json(result);
+})
